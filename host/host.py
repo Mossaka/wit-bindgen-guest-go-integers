131c3635b41d821f0752ee335766711796cb47eb
Jiaxiao Zhou <jiazho@microsoft.com>
2022-09-22 22:45:23 -0700

    feat: added more examples
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- host/host.py
+++ host/host.py
@@ -1,14 +1,49 @@
+from exports import Exports
+from imports import add_imports_to_linker, Imports
+import math;
 import sys
 import wasmtime
-from integers import add_integers_to_linker, Integers
-from integers_export import IntegersExport
 
-class MyIntegers(Integers):
-    def a1(self, x: int) -> None:
-        print(f"a1: {x}")
+class MyImports:
+    def roundtrip_u8(self, a: int) -> int:
+        return a
 
-    def a2(self, x: int) -> None:
-        print(f"a2: {x}")
+    def roundtrip_s8(self, a: int) -> int:
+        return a
+
+    def roundtrip_u16(self, a: int) -> int:
+        return a
+
+    def roundtrip_s16(self, a: int) -> int:
+        return a
+
+    def roundtrip_u32(self, a: int) -> int:
+        return a
+
+    def roundtrip_s32(self, a: int) -> int:
+        return a
+
+    def roundtrip_u64(self, a: int) -> int:
+        return a
+
+    def roundtrip_s64(self, a: int) -> int:
+        return a
+
+    def roundtrip_float32(self, a: float) -> float:
+        return a
+
+    def roundtrip_float64(self, a: float) -> float:
+        return a
+
+    def roundtrip_char(self, a: str) -> str:
+        return a
+
+    def set_scalar(self, a: int) -> None:
+        self.scalar = a
+
+    def get_scalar(self) -> int:
+        return self.scalar
+
 
 def run(wasm_file: str) -> None:
     store = wasmtime.Store()
@@ -20,12 +55,52 @@ def run(wasm_file: str) -> None:
     wasi.inherit_stderr()
     store.set_wasi(wasi)
 
-    add_integers_to_linker(linker, store, MyIntegers())
-    wasm = IntegersExport(store, linker, module)
+    imports = MyImports()
+    add_imports_to_linker(linker, store, imports)
+    wasm = Exports(store, linker, module)
 
-    res = wasm.res1(store)
-    res = wasm.res2(store)
-    print(res)
+    wasm.test_imports(store)
+    assert(wasm.roundtrip_u8(store, 1) == 1)
+    assert(wasm.roundtrip_u8(store, (1 << 8) - 1) == (1 << 8) - 1)
+    assert(wasm.roundtrip_u16(store, 1) == 1)
+    assert(wasm.roundtrip_u16(store, (1 << 16) - 1) == (1 << 16) - 1)
+    assert(wasm.roundtrip_u32(store, 1) == 1)
+    assert(wasm.roundtrip_u32(store, (1 << 32) - 1) == (1 << 32) - 1)
+    assert(wasm.roundtrip_u64(store, 1) == 1)
+    assert(wasm.roundtrip_u64(store, (1 << 64) - 1) == (1 << 64) - 1)
+
+    assert(wasm.roundtrip_s8(store, 1) == 1)
+    assert(wasm.roundtrip_s8(store, (1 << (8 - 1) - 1)) == (1 << (8 - 1) - 1))
+    assert(wasm.roundtrip_s8(store, -(1 << (8 - 1))) == -(1 << (8 - 1)))
+    assert(wasm.roundtrip_s16(store, 1) == 1)
+    assert(wasm.roundtrip_s16(store, (1 << (16 - 1) - 1)) == (1 << (16 - 1) - 1))
+    assert(wasm.roundtrip_s16(store, -(1 << (16 - 1))) == -(1 << (16 - 1)))
+    assert(wasm.roundtrip_s32(store, 1) == 1)
+    assert(wasm.roundtrip_s32(store, (1 << (32 - 1) - 1)) == (1 << (32 - 1) - 1))
+    assert(wasm.roundtrip_s32(store, -(1 << (32 - 1))) == -(1 << (32 - 1)))
+    assert(wasm.roundtrip_s64(store, 1) == 1)
+    assert(wasm.roundtrip_s64(store, (1 << (64 - 1) - 1)) == (1 << (64 - 1) - 1))
+    assert(wasm.roundtrip_s64(store, -(1 << (64 - 1))) == -(1 << (64 - 1)))
+
+    # inf = float('inf')
+    # assert(wasm.roundtrip_float32(store, 1.0) == 1.0)
+    # assert(wasm.roundtrip_float32(store, inf) == inf)
+    # assert(wasm.roundtrip_float32(store, -inf) == -inf)
+    # assert(math.isnan(wasm.roundtrip_float32(store, float('nan'))))
+
+    # assert(wasm.roundtrip_float64(store, 1.0) == 1.0)
+    # assert(wasm.roundtrip_float64(store, inf) == inf)
+    # assert(wasm.roundtrip_float64(store, -inf) == -inf)
+    # assert(math.isnan(wasm.roundtrip_float64(store, float('nan'))))
+
+    assert(wasm.roundtrip_char(store, 'a') == 'a')
+    assert(wasm.roundtrip_char(store, ' ') == ' ')
+    assert(wasm.roundtrip_char(store, '🚩') == '🚩')
+
+    wasm.set_scalar(store, 2)
+    assert(wasm.get_scalar(store) == 2)
+    wasm.set_scalar(store, 4)
+    assert(wasm.get_scalar(store) == 4)
 
 if __name__ == '__main__':
     run(sys.argv[1])
--- main.go
+++ main.go
@@ -1,21 +1,149 @@
 package main
 
 import (
-	"github.com/mossaka/go-wit-bindgen-integers/integers"
-	"github.com/mossaka/go-wit-bindgen-integers/integers_export"
-	// "fmt"
+	"math"
+
+	"github.com/mossaka/go-wit-bindgen-integers/exports"
+	"github.com/mossaka/go-wit-bindgen-integers/imports"
 )
 
+var SCALAR uint32 = 0
+
 func init() {
-	integers_export.SetIntegersExport(IntegersExportImpl{})
+	exports.SetExports(IntegersExportImpl{})
 }
 
-type IntegersExportImpl struct {}
+type IntegersExportImpl struct{}
 
-func (i IntegersExportImpl) Res() uint8 {
-	return 42
+func (i IntegersExportImpl) TestImports() {
+	if imports.RoundtripU8(1) != 1 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripU8(math.MaxUint8) != math.MaxUint8 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripU16(math.MaxUint16) != math.MaxUint16 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripU32(math.MaxUint32) != math.MaxUint32 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripU64(math.MaxUint64) != math.MaxUint64 {
+		panic("roundtrip test failed")
+	}
+
+	if imports.RoundtripS8(math.MaxInt8) != math.MaxInt8 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripS16(math.MaxInt16) != math.MaxInt16 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripS32(math.MaxInt32) != math.MaxInt32 {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripS64(math.MaxInt64) != math.MaxInt64 {
+		panic("roundtrip test failed")
+	}
+
+	// if imports.RoundtripS8(math.MinInt8) != math.MinInt8 {
+	// 	panic("roundtrip test failed")
+	// }
+	// if imports.RoundtripS16(math.MinInt16) != math.MinInt16 {
+	// 	panic("roundtrip test failed")
+	// }
+	// if imports.RoundtripS32(math.MinInt32) != math.MinInt32 {
+	// 	panic("roundtrip test failed")
+	// }
+	// if imports.RoundtripS64(math.MinInt64) != math.MinInt64 {
+	// 	panic("roundtrip test failed")
+	// }
+
+	// if imports.RoundtripFloat32(1.0) != 1.0 {
+	// 	panic("roundtrip test failed")
+	// }
+	// if imports.RoundtripFloat64(1.0) != 1.0 {
+	// 	panic("roundtrip test failed")
+	// }
+	// if imports.RoundtripFloat32(math.MaxFloat32) != math.MaxFloat32 {
+	// 	panic("roundtrip test failed")
+	// }
+	// if imports.RoundtripFloat64(math.MaxFloat64) != math.MaxFloat64 {
+	// 	panic("roundtrip test failed")
+	// }
+
+	if imports.RoundtripChar('a') != 'a' {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripChar('z') != 'z' {
+		panic("roundtrip test failed")
+	}
+	if imports.RoundtripChar('A') != 'A' {
+		panic("roundtrip test failed")
+	}
+
+	imports.SetScalar(2)
+	if imports.GetScalar() != 2 {
+		panic("scalar test failed")
+	}
+
+	imports.SetScalar(4)
+	if imports.GetScalar() != 4 {
+		panic("scalar test failed")
+	}
+
+}
+
+func (i IntegersExportImpl) RoundtripU8(a uint8) uint8 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripS8(a int8) int8 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripU16(a uint16) uint16 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripS16(a int16) int16 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripU32(a uint32) uint32 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripS32(a int32) int32 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripU64(a uint64) uint64 {
+	return a
+}
+
+func (i IntegersExportImpl) RoundtripS64(a int64) int64 {
+	return a
+}
+
+// func (i IntegersExportImpl) RoundtripFloat32(a float32) float32 {
+// 	return a
+// }
+
+// func (i IntegersExportImpl) RoundtripFloat64(a float64) float64 {
+// 	return a
+// }
+
+func (i IntegersExportImpl) RoundtripChar(a uint32) uint32 {
+	return a
+}
+
+func (i IntegersExportImpl) SetScalar(a uint32) {
+	SCALAR = a
+}
+
+func (i IntegersExportImpl) GetScalar() uint32 {
+	return SCALAR
 }
 
 func main() {
-	http.get().put()
 }
